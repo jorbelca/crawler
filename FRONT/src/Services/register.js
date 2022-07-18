cec66c78b3ff21ade9ec2945d4c327cd47b7de65
jorbelca <76847923+jorbelca@users.noreply.github.com>
2022-07-18 17:06:39 +0200

    Add files via upload

--- FRONT/src/Services/register.js
+++ FRONT/src/Services/register.js
@@ -8,8 +8,9 @@ export const registerUser = async (email, username, password) => {
       mode: "cors",
       body: JSON.stringify({ username: username, email: email, password: password }),
       headers: {
-        "Content-Type": "application/json; charset=UTF-8 ",
-        Accept: "application/json",
+        "Content-Type": "application/json ",
+        "Accept": "*/*",
+        "Connection": "keep-alive"
       },
     })
     return response
