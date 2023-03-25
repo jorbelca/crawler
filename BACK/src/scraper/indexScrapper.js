52bb3d6f318a25630b59c1ec277c66874fef049f
jorbelca <76847923+jorbelca@users.noreply.github.com>
2023-03-25 11:23:52 +0100

    Dockerfile added

--- BACK/src/scraper/indexScrapper.js
+++ BACK/src/scraper/indexScrapper.js
@@ -1,41 +1,43 @@
-import puppeteer from 'puppeteer-extra'
-import StealthPlugin from'puppeteer-extra-plugin-stealth'
+import puppeteer from "puppeteer-extra"
+import StealthPlugin from "puppeteer-extra-plugin-stealth"
 puppeteer.use(StealthPlugin())
+import "dotenv/config"
 
-import { executablePath } from'puppeteer'
+import { executablePath } from "puppeteer"
 
-
-export const urlData = async function run(url,selector) {
+export const urlData = async function run(url, selector) {
   // const urlHost = await transformURL(url)
   // { urlHost !== undefined ? urlHost : '' }
   try {
-    
-  const browser = await puppeteer.launch({
-    headless: true, executablePath: executablePath(),args: ['--no-sandbox','--disable-setuid-sandbox'] 
-  });
-  const page = await browser.newPage();
+    const browser = await puppeteer.launch({
+      headless: true,
+      executablePath:
+        process.env.NODE_ENV === "production"
+          ? process.env.PUPPETEER_EXECUTABLE_PATH
+          : executablePath(),
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--single-process",
+        "--no-zygote",
+      ],
+    })
+    const page = await browser.newPage()
 
-  await page.goto(url);
-  await page.setViewport({ width: 1080, height: 1024 });
-  await page.waitForSelector('body', 5000)
+    await page.goto(url)
+    await page.setViewport({ width: 1080, height: 1024 })
+    await page.waitForSelector("body", 5000)
 
-  const html = await page.$eval(selector, el => el.innerText)
+    const html = await page.$eval(selector, (el) => el.innerText)
 
-
-
-  await browser.close();
-  return html
-
+    await browser.close()
+    return html
   } catch (error) {
-    console.log(error);
+    console.log(error)
     return error
   }
+}
 
-};
-
-
-
-
-
-
- const transformURL = (url) => { if (url) return url.match(/\b((www)\.)[-A-Z0-9+&@#%?=~_|$!:,.;]*/ig)[0] }
+const transformURL = (url) => {
+  if (url) return url.match(/\b((www)\.)[-A-Z0-9+&@#%?=~_|$!:,.;]*/gi)[0]
+}
